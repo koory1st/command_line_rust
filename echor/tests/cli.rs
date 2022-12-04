466d2961690eaafe482805850f5392dd9ae88eec
koory1st <32436334@qq.com>
2022-12-04 19:12:14 +0800

    feat: add hello stdout test

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -1,10 +1,28 @@
+use std::fs;
+
 use assert_cmd::Command;
 use predicates::prelude::*;
 
+const COMMAND_NAME: &'static str = "echor";
+
 #[test]
 fn dies_no_args() {
-    let mut command = Command::cargo_bin("echor").unwrap();
+    let mut command = Command::cargo_bin(COMMAND_NAME).unwrap();
     command.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+}
+
+#[test]
+fn runs() {
+    let mut command = Command::cargo_bin(COMMAND_NAME).unwrap();
+    command.arg("test").assert().success();
+}
+
+#[test]
+fn hello1() {
+    let outfile = "tests/expected/hello1.txt";
+    let expected = fs::read_to_string(outfile).unwrap();
+    let mut command = Command::cargo_bin(COMMAND_NAME).unwrap();
+    command.arg("Hello there").assert().success().stdout(expected);
 }
