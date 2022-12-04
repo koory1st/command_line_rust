141cb4c56c7701057504e68fa3fad9fb4398eab0
koory1st <32436334@qq.com>
2022-12-04 19:18:18 +0800

    feat: add test hello2

--- echor/tests/cli.rs
+++ echor/tests/cli.rs
@@ -5,26 +5,45 @@ use predicates::prelude::*;
 
 const COMMAND_NAME: &'static str = "echor";
 
+type TestResult = Result<(), Box<dyn std::error::Error>>;
+
 #[test]
-fn dies_no_args() {
-    let mut command = Command::cargo_bin(COMMAND_NAME).unwrap();
+fn dies_no_args() -> TestResult {
+    let mut command = Command::cargo_bin(COMMAND_NAME)?;
     command.assert()
         .failure()
         .stderr(predicate::str::contains("USAGE"));
+
+    Ok(())
 }
 
 #[test]
-fn runs() {
+fn runs() -> TestResult {
     let mut command = Command::cargo_bin(COMMAND_NAME).unwrap();
     command.arg("test").assert().success();
+
+    Ok(())
 }
 
 #[test]
-fn hello1() -> Result<(), Box<dyn std::error::Error>> {
+fn hello1() -> TestResult {
     let outfile = "tests/expected/hello1.txt";
     let expected = fs::read_to_string(outfile)?;
     let mut command = Command::cargo_bin(COMMAND_NAME)?;
     command.arg("Hello there").assert().success().stdout(expected);
+
+    Ok(())
+}
+
+#[test]
+fn hello2() -> TestResult {
+    let outfile = "tests/expected/hello2.txt";
+    let expected = fs::read_to_string(outfile)?;
+    let mut command = Command::cargo_bin(COMMAND_NAME)?;
+    command.args(vec!["Hello", "there"])
+        .assert()
+        .success()
+        .stdout(expected);
 
     Ok(())
 }
