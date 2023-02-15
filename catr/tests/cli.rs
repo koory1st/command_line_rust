134a0f65e6c72f54a13da3a146670db8a6a09461
levy <levy@mxlinux>
2023-02-15 23:01:42 +0800

    test: add no file

--- catr/tests/cli.rs
+++ catr/tests/cli.rs
@@ -1,7 +1,8 @@
-use std::error::Error;
+use std::{error::Error, fs};
 
 use assert_cmd::Command;
 use predicates::prelude::predicate;
+use rand::{distributions::Alphanumeric, Rng};
 
 type TestResult = Result<(), Box<dyn Error>>;
 
@@ -15,5 +16,31 @@ fn usage() -> TestResult {
             .assert()
             .stdout(predicate::str::contains("Usage"));
     }
+    Ok(())
+}
+
+fn gen_bad_file() -> String {
+    loop {
+        let filename: String = rand::thread_rng()
+            .sample_iter(&Alphanumeric)
+            .take(7)
+            .map(char::from)
+            .collect();
+
+        if fs::metadata(&filename).is_err() {
+            return filename;
+        }
+    }
+}
+#[test]
+fn skips_bad_file() -> TestResult {
+    let bad = gen_bad_file();
+    let expected = format!("{}: .* [(]os error 2[)]", bad);
+    Command::cargo_bin(PRG)?
+        .arg(&bad)
+        .assert()
+        .success()
+        .stderr(predicate::str::is_match(expected)?);
+
     Ok(())
 }
