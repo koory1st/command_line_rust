1e116f9af2bb66504746f065533fd71d888233c0
levy <levy@mxlinux>
2023-02-21 22:34:14 +0800

    feat: add

--- headr/src/lib.rs
+++ headr/src/lib.rs
@@ -4,6 +4,7 @@ use clap::{Arg, ArgAction, Command};
 
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
+#[derive(Debug)]
 pub struct Config {
     files: Vec<String>,
     lines: usize,
@@ -23,25 +24,70 @@ pub fn get_args() -> MyResult<Config> {
         )
         .arg(
             Arg::new("lines")
+                .value_name("lines")
+                .long("lines")
                 .short('n')
-                .help("print the first K bytes of each file"),
+                .default_value("10")
+                .help("Number of lines"),
         )
         .arg(
             Arg::new("bytes")
+                .value_name("bytes")
+                .long("bytes")
                 .short('c')
-                .help("print the first K lines instead of the first 10;"),
+                .help("Number of bytes"),
         )
         .get_matches();
 
     let files: Vec<String> = matches
-        .get_many::<String>("FILES")
+        .get_many::<String>("files")
         .unwrap_or_default()
         .map(|v| v.to_owned())
         .collect::<Vec<_>>();
 
+    // let lines = matches
+    // .get_one("lines")
+    //     .ma
+
     Ok(Config {
         files: files,
-        lines: (),
-        bytes: (),
+        lines: 1,
+        bytes: Some(1),
     })
+}
+
+pub fn run(args: Config) -> MyResult<()> {
+    dbg!(&args);
+    Ok(())
+}
+
+fn parse_positive_int(val: &str) -> MyResult<usize> {
+    match val.parse() {
+        Ok(v) => {
+            if v > 0 {
+                Ok(v)
+            } else {
+                Err(val.into())
+            }
+        }
+        _ => Err(val.into()),
+    }
+}
+
+#[test]
+fn test_parse_positive_int() {
+    // 3 is an OK integer
+    let res = parse_positive_int("3");
+    assert!(res.is_ok());
+    assert_eq!(res.unwrap(), 3usize);
+
+    // Any string is an error
+    let res = parse_positive_int("a");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "a".to_owned());
+
+    // 0 is an error
+    let res = parse_positive_int("0");
+    assert!(res.is_err());
+    assert_eq!(res.unwrap_err().to_string(), "0".to_owned());
 }
